58becabefeedbe57e50748e750e7407593198ea8
Pedro Yvo Lucas <91639878+pedrokaa2@users.noreply.github.com>
2022-06-22 14:20:30 -0300

    feat: about page done

--- src/Pages/About/index.jsx
+++ src/Pages/About/index.jsx
@@ -1,6 +1,6 @@
-import { Link } from 'react-router-dom'
-import PedroLogo from '../../Assets/pedrodev 1.svg'
-import MinhaFoto from '../../Assets/fotinha.svg'
+import { Link } from "react-router-dom";
+import PedroLogo from "../../Assets/pedrodev 1.svg";
+import MinhaFoto from "../../Assets/fotinha.svg";
 import * as S from "./style";
 
 const About = () => {
@@ -8,32 +8,43 @@ const About = () => {
     <S.BoxDiv>
       <header>
         <Link to="/">
-          <img src={PedroLogo} alt="pedrologo"/>
+          <img src={PedroLogo} alt="pedrologo" />
         </Link>
       </header>
-      <main>
-      <S.DivSpan>
-          <S.DivVaziaCor>
-            <div></div>
-          </S.DivVaziaCor>
+      <S.BoxMain>
+        <S.BoxDivCss>
+          <S.DivSpan className="sobremim">
+            <S.DivVaziaCor>
+              <div></div>
+            </S.DivVaziaCor>
             <span>sobre mim</span>
-        </S.DivSpan>
-        <S.BoxDivCss>
-          <p>Oie! Sou Pedro Yvo Lucas! Um Desenvolvedor Front-End formado na Kenzie Academy Brasil, UX/UI designer e Desevolvedor Back-End em formação, sou entusiasta de tecnologias e procuro estar aprendendo cada vez mais, estou ingressando nessa carreira afim de adquirir mais conhecimentos e procurando experiências e oportunidades nesse mercado!</p>
-          <img src={MinhaFoto} alt="fotinha pedro"/>
+          </S.DivSpan>
+          <S.DivImgP>
+            <p>
+              Oie! Sou Pedro Yvo Lucas! Um Desenvolvedor Front-End formado na
+              Kenzie Academy Brasil, UX/UI designer e Desevolvedor Back-End em
+              formação, sou entusiasta de tecnologias e procuro estar aprendendo
+              cada vez mais, estou ingressando nessa carreira afim de adquirir
+              mais conhecimentos e procurando experiências e oportunidades nesse
+              mercado!
+            </p>
+              <img src={MinhaFoto} alt="fotinha pedro" />
+          </S.DivImgP>
         </S.BoxDivCss>
-        <S.DivSpan>
-          <S.DivVaziaCor>
-            <div></div>
-          </S.DivVaziaCor>
+        <S.BoxDivCss className="contatobox">
+          <S.DivSpan className="contato">
+            <S.DivVaziaCor>
+              <div></div>
+            </S.DivVaziaCor>
             <span>contato</span>
-        </S.DivSpan>
-        <S.BoxDivCss>
-          <p>email: pedkaa@yahoo.com</p>
+          </S.DivSpan>
+          <S.DivP>
+            <p>email: pedkaa@yahoo.com</p>
+          </S.DivP>
         </S.BoxDivCss>
-      </main>
+      </S.BoxMain>
     </S.BoxDiv>
-  )
-}
+  );
+};
 
-export default About
+export default About;
--- src/Pages/About/style.js
+++ src/Pages/About/style.js
@@ -3,26 +3,56 @@ import styled from "styled-components";
 export const BoxDiv = styled.div ` 
   width: 100vw;
 
+  header {
+    display: flex;
+    flex-direction: column;
+  }
+
+  a {
+    display: flex;
+    align-self: center;
+  }
+
   img {
     display: flex;
     flex-direction: column;
     justify-content: center;
     width: 300px;
-    margin-left: 10px;
+    margin-bottom: 30px;
+  }
+`
+
+export const BoxMain = styled.main `
+  display: flex;
+  flex-direction: column;
+
+  @media (min-width: 768px) {
+    position: relative;
+    
+    .contatobox { 
+      margin-right: 296px;
+    }
   }
 `
 
 export const BoxDivCss = styled.div `
   display: flex;
   flex-direction: column;
+  align-self: center;
 
   p {
-    margin: 30px 30px;
+    margin: 30px 0px;
     width: 250px;
+    align-self: center;
   }
 
-  @media (min-width: 500px) {
-    flex-direction: row;
+  img {
+    width: 250px;
+    align-self: center;
+    margin-left: 0;
+  }
+
+  @media (min-width: 768px) {
     justify-content: space-around;
 
     p {
@@ -30,6 +60,25 @@ export const BoxDivCss = styled.div `
       margin: auto 0;
       font-size: 20px;
     }
+
+    img {
+      width: 300px;
+    }
+
+  }
+
+`
+
+export const DivImgP = styled.div `
+  @media (min-width: 768px) {
+    display: flex;
+    justify-content: space-around;
+  }
+`
+
+export const DivP = styled.div `
+  @media (min-width: 768px) {
+    margin: 30px 0px;
   }
 `
 
@@ -37,19 +86,24 @@ export const DivVaziaCor = styled.div `
   background: var(--darkblue);
   width: 50px;
   height: 11px;
-  margin-left: 30px;
+  align-self: center;
   display: flex;
   align-items: center;
 `
 
 export const DivSpan = styled.div `
   display: flex;
-  align-items: center;
   margin-top: 20px;
+  align-self: center;
+  width: 250px;
 
   span {
     margin-left: 10px;
     width: 90px;
     text-align: center;
+  }
+
+  @media (min-width: 768px) {
+    align-self: start;
   }
 `
